361b927ee37bf00297de2ed9c17577d7c75468c2
Wucop228 <zhupa2006@mail.ru>
2025-04-28 16:08:31 +0300

    create dir

--- collect_files.sh
+++ collect_files.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+max_depth=""
+input_dir=""
+output_dir=""
+
+if [ "$1" = "--max_depth" ]; then
+    max_depth="$2"
+    input_dir="$3"
+    output_dir="$4"
+else
+    input_dir="$1"
+    output_dir="$2"
+fi
+
+mkdir -p "$output_dir"
+
