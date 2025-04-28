8ba6bb1ccb808dba74e31ae5b6657ae713d95b02
Wucop228 <zhupa2006@mail.ru>
2025-04-28 18:42:37 +0300

    fixed depth

--- collect_files.sh
+++ collect_files.sh
@@ -16,20 +16,17 @@ fi
 mkdir -p "$output_dir"
 
 python3 - <<EOF
-import os
 import shutil
+import os
 from pathlib import Path
 
 max_depth=None if "$max_depth" == "" else int("$max_depth")
 input_dir="$input_dir"
 output_dir="$output_dir"
 
-def get_all_files(directory):
+def get_all_files(dir):
     all_files = []
-    for dirpath, _, filenames in os.walk(directory):
-        current_depth = dirpath[len(input_dir):].count(os.path.sep)
-        if max_depth is not None and current_depth > max_depth: continue
-
+    for dirpath, _, filenames in os.walk(dir):
         for filename in filenames:
             all_files.append(os.path.join(dirpath, filename))
     return all_files
@@ -41,8 +38,7 @@ if max_depth is None:
 else:
     for filepath in get_all_files(input_dir):
         if os.path.isfile(filepath):
-            print(filepath)
-            print(os.path.join(output_dir, '/'.join(list(Path(filepath).parts)[1:-1])))
+            if len(list(Path(filepath).parts)[1:-1]) >= max_depth: continue
             os.makedirs(os.path.join(output_dir, '/'.join(list(Path(filepath).parts)[1:-1])), exist_ok=True)
             shutil.copy2(filepath, os.path.join(output_dir, '/'.join(list(Path(filepath).parts[1:]))))
 EOF
