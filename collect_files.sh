929e44351c139745fd45e686db7188b9da1d8629
Wucop228 <zhupa2006@mail.ru>
2025-04-28 17:19:56 +0300

    file without max_depth

--- collect_files.sh
+++ collect_files.sh
@@ -15,3 +15,27 @@ fi
 
 mkdir -p "$output_dir"
 
+python3 - <<EOF
+import os
+import shutil
+
+max_depth=None if "$max_depth" == "" else int("$max_depth")
+input_dir="$input_dir"
+output_dir="$output_dir"
+
+def get_all_files(directory):
+    all_files = []
+    for dirpath, _, filenames in os.walk(directory):
+        current_depth = dirpath[len(input_dir):].count(os.path.sep)
+        if max_depth is not None and current_depth > max_depth: continue
+
+        for filename in filenames:
+            all_files.append(os.path.join(dirpath, filename))
+    return all_files
+
+files = get_all_files(input_dir)
+
+for filepath in files:
+    if os.path.isfile(filepath):
+        shutil.copy2(filepath, os.path.join(output_dir, os.path.basename(filepath)))
+EOF
